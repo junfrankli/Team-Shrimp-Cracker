e6ec83e185078469278690f54fc39e5933e6a484
Malavika Attaluri <mpa46@cornell.edu>
2017-11-21 17:13:16 -0500

    DFS with treasure detection

--- sketches/DFS/DFS.ino
+++ sketches/DFS/DFS.ino
@@ -25,10 +25,14 @@ Servo right;
 #define forwardSlow 530
 #define uwot 500
 
-// line sensor threshold
-#define threshold 500
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
 
-int sensor0, sensor1, sensor2, sensor3, sensorBack, state = 0;
+#include <FFT.h> // include the library
+
+#define threshold 60
+
+int adc_state = 0;
 
 typedef struct square {
   int xPos;
@@ -58,6 +62,12 @@ void setup() {
   pinMode(sensor1pin, INPUT); 
   pinMode(sensor2pin, INPUT); 
   pinMode(sensor3pin, INPUT);
+
+ 
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  adc_state = ADCSRA; // save adc state
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
 
   Serial.begin(115200);
 }
@@ -151,13 +161,8 @@ void loop() {
             goalY = curY-1;
             break;
         }
-        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)) {  
+        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)) 
           frontier = push(frontier, goalX, goalY);
-          Serial.println("adding to frontier");
-          Serial.println("direction 3");
-          Serial.println(goalX);
-          Serial.println(goalY);
-        }
       }
       if (!detectWall(2)){
         switch(heading) {
@@ -178,13 +183,9 @@ void loop() {
             goalY = curY;
             break;
         }
-        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)) {
+        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)) 
           frontier = push(frontier, goalX, goalY);
-          Serial.println("adding to frontier");
-          Serial.println("direction 2");
-          Serial.println(goalX);
-          Serial.println(goalY);
-        }
+        
       }
       if (!detectWall(1)) {
         switch(heading) {
@@ -205,14 +206,8 @@ void loop() {
             goalY = curY+1;
             break;
         }
-        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)){
+        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY))
           frontier = push(frontier, goalX, goalY);
-          Serial.println("adding to frontier");
-          Serial.println("direction 1");
-          Serial.println(goalX);
-          Serial.println(goalY);
-        }
-        
       }
       if (!detectWall(0)) {
         switch(heading) {
@@ -233,17 +228,15 @@ void loop() {
             goalY = curY;
             break;
         }
-        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)) {
+        if (!findsquare(visited, goalX, goalY) && !findsquare(frontier, goalX, goalY)) 
           frontier = push(frontier, goalX, goalY);
-          Serial.println("adding to frontier");
-          Serial.println("direction 0");
-          Serial.println(goalX);
-          Serial.println(goalY);
-        }
       } 
+      if (fft() > 0) 
+        digitalWrite(13, HIGH);
+      else 
+        digitalWrite(13, LOW); 
       free(frontier_loc);
     }
-    delay(100);
   }
   left.write(92);
   right.write(92);
@@ -484,4 +477,82 @@ void writeL(int s) {
   left.writeMicroseconds(1000 + s);
 } 
 
+
+int fft() {
+   int sum = 0;
+   ADCSRA = 0xe5;
+   for (int i = 0; i < 10; i++) {
+    cli();  // UDRE interrupt slows this way down on arduino1
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei(); // turns interrupts back on
+    int x = treasureDetect();
+    Serial.println(x);
+    sum = sum + x;
+   }
+   ADCSRA = adc_state;
+   Serial.println("sum");
+   Serial.println(sum);
+   if (sum < 5)
+    return 0;
+   else if (sum < 15)
+    return 1;
+   else if (sum < 25)
+    return 2;
+   else return 3; 
+}
+
+//treasure detection
+int treasureDetect() {
+  int treasure_7;
+  int treasure_12;
+  int treasure_17;
+  int max_7_bin = 0;
+  for (int i = 0; i < 256; i++) {
+    Serial.println (i);
+    Serial.println ("DONE with fft");
+  }
+    for (int i = 46; i < 50; i++) {
+      int x = (int) fft_log_out[i];
+      if (x > max_7_bin)
+        max_7_bin = x;
+    }
+    int max_12_bin = 0;
+    for (int i = 80; i < 83; i++) {
+      int x = (int) fft_log_out[i];
+      if(x > max_12_bin) 
+        max_12_bin = x;
+    }
+    int max_17_bin = 0;
+    for (int i = 113; i < 116; i++) {
+      int x = (int) fft_log_out[i];
+      if (x > max_17_bin)
+        max_17_bin = x;
+    }
+    if (max_17_bin > max_12_bin && max_17_bin > max_7_bin) {
+      if (max_17_bin > threshold) 
+       return 3;
+    }
+    else if (max_12_bin > max_7_bin) {
+      if (max_12_bin > threshold)  
+       return 2;
+    }
+    else 
+      if (max_7_bin > threshold)
+        return 1;
+    return 0;
+}
 
